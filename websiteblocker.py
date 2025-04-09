57f8e88d82168d6bb7db4867a04a18b86800301f
tanvi23kamath <tanvikamath23@gmail.com>
2025-04-09 23:19:48 +0530

    Update websiteblocker.py

--- websiteblocker.py
+++ websiteblocker.py
@@ -1,11 +1,10 @@
 import time
 from datetime import datetime
 
-# Path to the hosts file (Windows )
-hosts_path = r"C:\Windows\System32\drivers\etc\hosts"  # Windows
-
-redirect_ip = "127.0.0.1"  # Redirects the blocked websites
-blocked_websites = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.com"]
+# Path to the hosts file (Windows)
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect_ip = "127.0.0.1"
+blocked_websites = ["www.facebook.com", "facebook.com"]
 
 # Define blocking hours (e.g., 9 PM to 5 AM)
 start_hour = 21
@@ -14,10 +13,17 @@ end_hour = 5
 while True:
     current_hour = datetime.now().hour
 
+    # Determine if current time is within the blocking period
+    if start_hour <= end_hour:
+        is_block_time = start_hour <= current_hour < end_hour
+    else:
+        is_block_time = current_hour >= start_hour or current_hour < end_hour
+
     with open(hosts_path, "r+") as file:
         content = file.read()
+        file.seek(0)
 
-        if start_hour <= current_hour < end_hour:
+        if is_block_time:
             print("Blocking websites...")
             for website in blocked_websites:
                 if website not in content:
@@ -25,10 +31,10 @@ while True:
         else:
             print("Unblocking websites...")
             lines = content.splitlines()
-            file.seek(0)
             for line in lines:
                 if not any(website in line for website in blocked_websites):
                     file.write(line + "\n")
             file.truncate()
 
     time.sleep(60)  # Check every 60 seconds
+
